f22d10a8983ca64410dcdb2f0ca60c9628bc7a73
Unknown <bourouis.sabrina@laposte.net>
2017-06-29 09:59:16 +0200

    Course project until section 10
    Done:
     - The basics
     - Components & Databinding
     - Directives
     - Services & Dependency Injection

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,29 @@
+import {Recipe} from './recipe.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'http://www.manjulaskitchen.com/blog/wp-content/uploads/vermicelli_upma_spicy_noodles.jpg',
+      [
+        new Ingredient('Tomatoes', 3),
+        new Ingredient('Pasta', 5)
+      ])
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
